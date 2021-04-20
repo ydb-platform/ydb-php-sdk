23e1648c3430dc09fa27d263b15f1559b0c6acda
Alexei Shabalin <alshabalin@yandex.ru>
2021-04-20 15:14:17 +0800

    typeValue helper datetime typo fix

--- src/Traits/TypeValueHelpersTrait.php
+++ src/Traits/TypeValueHelpersTrait.php
@@ -73,7 +73,7 @@ trait TypeValueHelpersTrait
         }
         else if (is_string($value) && preg_match('/^\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}$/', $value))
         {
-            return new DateType($value);
+            return new DatetimeType($value);
         }
         else if (is_array($value))
         {
