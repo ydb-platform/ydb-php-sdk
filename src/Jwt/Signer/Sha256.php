db454a8a852ce967325be570ea29c5144974a993
Alexei Shabalin <alshabalin@yandex.ru>
2021-07-14 17:27:20 +0800

    phpseclib may now be 2.0 or 3.0 version

--- src/Jwt/Signer/Sha256.php
+++ src/Jwt/Signer/Sha256.php
@@ -5,6 +5,7 @@ namespace YandexCloud\Ydb\Jwt\Signer;
 use Lcobucci\JWT\Signer\Key as SignerKey;
 use Lcobucci\JWT\Signer\Rsa as SignerRsa;
 
+use phpseclib\Crypt\RSA as LegacyRSA;
 use phpseclib3\Crypt\RSA;
 use phpseclib3\Crypt\PublicKeyLoader;
 
@@ -17,13 +18,25 @@ class Sha256 extends SignerRsa
      */
     public function createHash($payload, SignerKey $key)
     {
-        $private = PublicKeyLoader::load($key->getContent());
+        $keyContent = $key->getContent();
 
-        $signature = $private
-            ->withPadding(RSA::SIGNATURE_PSS)
-            ->sign($payload);
+        if (class_exists(LegacyRSA::class))
+        {
+            $rsa = new LegacyRSA;
+            $rsa->loadKey($keyContent);
+            $rsa->setHash('sha256');
+            $rsa->setMGFHash('sha256');
+            $rsa->setSignatureMode(LegacyRSA::SIGNATURE_PSS);
 
-        return $signature;
+            return $rsa->sign($payload);
+        }
+        else
+        {
+            $rsa = PublicKeyLoader::load($keyContent);
+            $rsa->withPadding(RSA::SIGNATURE_PSS);
+
+            return $rsa->sign($payload);
+        }
     }
 
     /**
