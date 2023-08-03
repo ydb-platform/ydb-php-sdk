b50722f8e4550e0e6e55b98cc87a451ccfe0cebc
Илья <ilyakharev@ydb.tech>
2023-08-03 11:52:27 +0300

    Update CreateCommand.php

--- slo-workload/src/commands/CreateCommand.php
+++ slo-workload/src/commands/CreateCommand.php
@@ -63,23 +63,42 @@ class CreateCommand extends \YdbPlatform\Ydb\Slo\Command
             "partitionSize"    => $partitionSize,
         ]);
 
-        $table = new YdbTable();
-        $partitionSettings = [];
-        $partitionSettings['partitioning_by_size'] = $partitionSize;
-        $partitionSettings['min_partitions_count'] = $minPartitionsCount;
-        $partitionSettings['max_partitions_count'] = $maxPartitionsCount;
-        $table->partitionSettings($partitionSettings);
-        $table->addColumn('hash', 'UINT64');
-        $table->addColumn('id', 'UINT64');
-        $table->addColumn('payload_str', 'UTF8');
-        $table->addColumn('payload_double', 'DOUBLE');
-        $table->addColumn('payload_timestamp', 'TIMESTAMP');
-        $table->addColumn('payload_hash', 'UINT64');
-        $table->primaryKey(['hash', 'id']);
-        $table->compactionPolicy('small_table');
+//        $table = new YdbTable();
+//        $partitionSettings = [];
+//        $partitionSettings['partitioning_by_size'] = $partitionSize;
+//        $partitionSettings['min_partitions_count'] = $minPartitionsCount;
+//        $partitionSettings['max_partitions_count'] = $maxPartitionsCount;
+//        $table->partitionSettings($partitionSettings);
+//        $table->addColumn('hash', 'UINT64');
+//        $table->addColumn('id', 'UINT64');
+//        $table->addColumn('payload_str', 'UTF8');
+//        $table->addColumn('payload_double', 'DOUBLE');
+//        $table->addColumn('payload_timestamp', 'TIMESTAMP');
+//        $table->addColumn('payload_hash', 'UINT64');
+//        $table->primaryKey(['hash', 'id']);
+//        $table->compactionPolicy('small_table');
+//
+//        $ydb->table()->retrySession(function (Session $session) use ($table, $tableName) {
+//            $session->createTable($tableName, $table, ['hash', 'id']);
+//        });
 
-        $ydb->table()->retrySession(function (Session $session) use ($table, $tableName) {
-            $session->createTable($tableName, $table, ['hash', 'id']);
+        $table->retrySession(function (Session $session) use ($tableName) {
+            $session->query("CREATE TABLE `$tableName`
+(
+    `hash` Uint64,
+    `id` Uint64,
+    `payload_double` Double,
+    `payload_hash` Uint64,
+    `payload_str` Utf8,
+    `payload_timestamp` Timestamp,
+    PRIMARY KEY (`hash`, `id`)
+)
+WITH(
+    AUTO_PARTITIONING_MIN_PARTITIONS_COUNT = 6,
+    AUTO_PARTITIONING_MIN_PARTITIONS_COUNT = 1000,
+    AUTO_PARTITIONING_PARTITION_SIZE_MB = 1
+);
+");
         });
 
         $ydb->table()->getLogger()->info("Table created");
