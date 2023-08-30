17bebf1395f53af77ac77d93ccf233a54d733585
Илья <ilyakharev@ydb.tech>
2023-08-30 13:44:55 +0300

    Added reset in RunCommand.php

--- slo-workload/src/Commands/RunCommand.php
+++ slo-workload/src/Commands/RunCommand.php
@@ -137,6 +137,23 @@ Options:
             pcntl_waitpid($pid, $status);
             unset($pIds[$pid]);
         }
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $promPgw . '/metrics/job/workload-php/sdk/php/sdkVersion/' . Ydb::VERSION,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'DELETE',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
     }
 
     function forkJob(Closure $function, int $count): array
