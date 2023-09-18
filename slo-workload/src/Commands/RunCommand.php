d1395f5a16626164d00e6fd650277a4ebe8922a1
Илья <ilyakharev@ydb.tech>
2023-09-18 08:30:10 +0300

    Add sleep before run fork

--- slo-workload/src/Commands/RunCommand.php
+++ slo-workload/src/Commands/RunCommand.php
@@ -124,11 +124,13 @@ Options:
         $pIds = array_merge($pIds, $metricsPIds);
 
         $readPIds = $this->forkJob(function (int $i) use ($endpoint, $path, $tableName, $initialDataCount, $time, $readTimeout, $shutdownTime, $startTime) {
+            usleep($i*Defaults::RPS_PER_READ_FORK*1000);
             $this->readJob($endpoint, $path, $tableName, $initialDataCount, $time, $readTimeout, $i, $shutdownTime, $startTime);
         }, $readForks);
         $pIds = array_merge($pIds, $readPIds);
 
         $writePIds = $this->forkJob(function (int $i) use ($endpoint, $path, $tableName, $initialDataCount, $time, $writeTimeout, $shutdownTime, $startTime) {
+            usleep($i*Defaults::RPS_PER_WRITE_FORK*1000);
             $this->writeJob($endpoint, $path, $tableName, $initialDataCount, $time, $writeTimeout, $i, $shutdownTime, $startTime);
         }, $writeForks);
         $pIds = array_merge($pIds, $writePIds);
