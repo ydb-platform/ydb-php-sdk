d73d7323d58ea6257d822875a48cb2b8fda69fb8
Илья <ilyakharev@ydb.tech>
2023-09-18 09:46:46 +0300

    Add sleep before run fork

--- slo-workload/src/Defaults.php
+++ slo-workload/src/Defaults.php
@@ -11,8 +11,8 @@ class Defaults
 
     const GENERATOR_DATA_COUNT = 1000;
 
-    const RPS_PER_READ_FORK = 100;
-    const RPS_PER_WRITE_FORK = 50;
+    const RPS_PER_READ_FORK = 1;
+    const RPS_PER_WRITE_FORK = 1;
 
     const READ_RPS = 1000;
     const READ_TIMEOUT = 70; // milliseconds
